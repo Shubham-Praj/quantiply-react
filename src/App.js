e9aef5a4fc18817230517be7a63d235b8d22da0d
shubham-praj <shubhampraj11@gmail.com>
2022-03-14 02:28:08 +0530

    React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [apodData, setapodData] = useState([]);
+
+  async function getAPODData(reqDate) {
+    console.log(reqDate);
+
+    const reqConfig = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ date: reqDate }),
+    };
+
+    const response = await fetch(process.env.REACT_APP_URL, reqConfig);
+    const data = await response.json();
+    console.log(data.data[0]);
+    setapodData(data.data[0]);
+  }
+
+  useEffect(() => {
+     getAPODData("2019-12-01");
+  },[]);
+
+  console.log(apodData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="titleDiv">
+        <h1>Title :- {apodData.title}</h1>
+        <input
+          type="date"
+          onChange={date => getAPODData(date.target.value)}
+          // onChange={(e) => test(e.target.value)}
+          className="datePicker"
+        />
+      </div>
+      <div>
+        <img className="imageDiv" src={apodData.image_url} alt="image"></img>
+      </div>
+      <div>
+        <h3>Explanations :- </h3>
+        <h4>{apodData.explanation}</h4>
+      </div>
     </div>
   );
 }
