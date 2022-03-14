e2c8b2cd80eea4c2b45b8fd6d9d623aea39ad851
shubham-praj <shubhampraj11@gmail.com>
2022-03-15 00:47:01 +0530

    Image Exception handling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
 
 function App() {
   const [apodData, setapodData] = useState([]);
@@ -8,20 +9,20 @@ function App() {
     console.log(reqDate);
 
     const reqConfig = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ date: reqDate }),
     };
 
     const response = await fetch(process.env.REACT_APP_URL, reqConfig);
     const data = await response.json();
-    console.log(data.data[0]);
-    setapodData(data.data[0]);
+    console.log(data.data);
+    setapodData(data.data);
   }
 
   useEffect(() => {
-     getAPODData("2019-12-01");
-  },[]);
+    getAPODData("2019-12-01");
+  }, []);
 
   console.log(apodData);
 
@@ -31,13 +32,20 @@ function App() {
         <h1>Title :- {apodData.title}</h1>
         <input
           type="date"
-          onChange={date => getAPODData(date.target.value)}
-          // onChange={(e) => test(e.target.value)}
+          onChange={(date) => getAPODData(date.target.value)}
           className="datePicker"
         />
       </div>
       <div>
-        <img className="imageDiv" src={apodData.image_url} alt="image"></img>
+        {apodData.media_type === "image" ? (
+          <img
+            className="imageDiv"
+            src={apodData.image_url || apodData.url || apodData.hdurl}
+            alt="image"
+          ></img>
+        ) : (
+          <ReactPlayer url={apodData.image_url || apodData.url || apodData.hdurl}></ReactPlayer>
+        )}
       </div>
       <div>
         <h3>Explanations :- </h3>
